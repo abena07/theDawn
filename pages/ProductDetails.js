ce174b7f8e40666e37c884224c855ca62d5ccf15
Abena <abenabennett@gmail.com>
2022-06-14 15:27:13 +0000

    product details skeleton

--- pages/ProductDetails.js
+++ pages/ProductDetails.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
 
 
 export default function Home({ navigation }) {
@@ -8,14 +10,32 @@ export default function Home({ navigation }) {
   return (
     <View
       style={{
-        backgroundColor: "white",
+        backgroundColor:"#F6F6F6",
         flex: 1,
         paddingTop: 55,
-        paddingHorizontal: 20,
+        
       }}
     >
-        <Text>Babe</Text>
-     
+        <View style={styles.second}>
+            <Ionicons name="chevron-back" size={24} color="black" />
+            <Ionicons name="heart-outline" size={24} color="black" />          
+        </View>
+
+        <Image
+        style={{
+          width: 200,
+          height: 200,
+          marginHorizontal:60,
+          marginVertical:30,
+          
+        }}
+        source={require("../images/nike(red).jpg")}
+      />
+
+        <View style={styles.third}>
+
+        </View>
+        
     </View>
     
   );
@@ -23,7 +43,20 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
 
- 
+ second:{
+     flexDirection:"row",
+     justifyContent:"space-between",
+     marginTop:20,
+     paddingHorizontal: 20,
+     
 
+ },
+ third:{
+     borderRadius:40,
+     backgroundColor:"white",
+     height:490,
+    
+     
+ }
   
 });
