7ca1b6e02938f817fd782e885d1defb15fb5d6ca
Abena <abenabennett@gmail.com>
2022-06-17 19:25:58 +0000

    firebase integration in progress

--- Core/Config.js
+++ Core/Config.js
@@ -3,14 +3,13 @@ import {getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCcg-Sm0OZUSqrqkuAbtizRSZwvBcII2kw",
-    authDomain: "thedawn-9743b.firebaseapp.com",
-    projectId: "thedawn-9743b",
-    storageBucket: "thedawn-9743b.appspot.com",
-    messagingSenderId: "528034181435",
-    appId: "1:528034181435:web:79c78ebf2c50cabbc8071e",
-    measurementId: "G-XKV6B6FHZ4"
-  };
+  apiKey: "AIzaSyDNj2rnHqtYwFlxr47s3nHmF0DvLDKzyFo",
+  authDomain: "thedawn-bd825.firebaseapp.com",
+  projectId: "thedawn-bd825",
+  storageBucket: "thedawn-bd825.appspot.com",
+  messagingSenderId: "454757433681",
+  appId: "1:454757433681:web:6dd209d5dd706ce40c9370"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
